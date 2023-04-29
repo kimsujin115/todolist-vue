89381aa4d71cad59fe351e74609d5b5dcebe3b35
kimsujin <sooojiny@gmail.com>
2023-04-29 21:48:13 +0900

    input value 로컬스토리지에 저장

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="inputBox">
-    <input type="text" placeholder="할 일을 입력해 주세요.">
-    <button class="btnAdd">등록하기</button>
+    <input type="text" v-model="newTodoItem" @keyup.enter="onAddTodo" placeholder="할 일을 입력해 주세요.">
+    <button @click="onAddTodo" class="btnAdd">등록하기</button>
   </div>
 </template>
 
 <script>
     export default {
+      data() {
 
+        return {
+          newTodoItem : ''
+        }
+      },
+      methods : {
+        onAddTodo() {
+          if ( this.newTodoItem !== '') {
+            const item = this.newTodoItem;
+            localStorage.setItem(item, item); //로컬 스토리지에 데이터 저장
+            this.newTodoItem = '';
+
+          }
+        }
+      }
     }
 </script>
